f703d46022d9823ffa8394d0d22fc188a8130a46
Damian Niemczyk <djniemczyk7@googlemail.com>
2016-02-19 21:45:48 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject rpsls-clj "0.1.0-SNAPSHOT"
+  :description "A implementation of the quite famous
+                rock-paper-scissors-lizzard-spock game"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot rpsls-clj.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2"]]}})
