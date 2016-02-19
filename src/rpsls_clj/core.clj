5e8a28c327a780b39a21506bbff02060ae02c238
Damian Niemczyk <djniemczyk7@googlemail.com>
2016-02-19 22:30:41 +0100

    Create winner function for rock player

--- src/rpsls_clj/core.clj
+++ src/rpsls_clj/core.clj
@@ -1,5 +1,26 @@
+;;;; Rock Paper Scissors Lizzard Spock Game
+;;;; --------------------------------------
+;;;;
+;;;; A simple Clojure implementation of the famous game
+;;;; of hand-waving combat
 (ns rpsls-clj.core
   (:gen-class))
+
+;;; Any of the two players can choose one of the 5 options
+;;; "r" for (r)ock, "p" for (p)aper, "s" for (s)cissors,
+;;; "l" for (l)izzard, "o" for Sp(o)ck
+(defn winner
+  "Defines the wining guess
+   Returns 1 if the 1st guess wins, 2 if the 2nd one wins
+   and 0 in case of a draw"
+  [guess1 guess2]
+  (let [guesses [guess1 guess2]]
+    (cond
+      (= guesses ["r" "r"]) 0
+      (= guesses ["r" "p"]) 2
+      (= guesses ["r" "s"]) 1
+      (= guesses ["r" "l"]) 1
+      (= guesses ["r" "o"]) 2)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/rpsls_clj/core_test.clj
+++ test/rpsls_clj/core_test.clj
@@ -1,7 +1,12 @@
 (ns rpsls-clj.core-test
-  (:require [clojure.test :refer :all]
+  (:require [midje.sweet :refer :all]
+            [clojure.test :refer :all]
             [rpsls-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "Winner is determined correctly"
+  (fact "for the first player playing rock"
+    (winner "r" "r") => 0
+    (winner "r" "p") => 2
+    (winner "r" "s") => 1
+    (winner "r" "l") => 1
+    (winner "r" "o") => 2))
